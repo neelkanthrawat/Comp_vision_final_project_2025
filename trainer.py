c276064a143e480d512f3cd0a5fb9f60845ebd45
neelkanthrawat <neelkanthrawat3992@gmail.com>
2025-09-06 18:33:01 +0200

    add code for later unfreezing of vit backbone

--- trainer.py
+++ trainer.py
@@ -5,6 +5,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 import os  # likely for model saving/loading
 from tqdm import tqdm
+from typing import Optional
 
 class trainer:
     def __init__(self, model, 
@@ -14,7 +15,9 @@ class trainer:
                 device='cuda',
                 use_trap_scheduler=False,
                 model_kwargs = None,
-                criterion_kwargs= {"num_classes": 2, "epsilon": 1e-6}):
+                criterion_kwargs= {"num_classes": 2, "epsilon": 1e-6},
+                want_backbone_frozen_intially: bool = False,
+                freeze_epochs: Optional[int] = None):
         """
     Initializes the trainer object with all necessary components for training and validation.
 
@@ -39,7 +42,9 @@ class trainer:
         
         self.model = model
         self.optimizer = optimizer
-        self.lr = lr
+        # learning rates of 2 different groups:
+        self.lr = lr# I think we need to remove this. we do not need it seperately
+        self.lr_groups = [pg['lr'] for pg in optimizer.param_groups]# self.lr_groups[0] is LR for backbone and self.lr_groups[1] is LR for head
         self.criterion = criterion
         self.criterion_kwargs= criterion_kwargs 
         self.num_epoch = num_epoch
@@ -49,6 +54,7 @@ class trainer:
         
         # Calculate and store dataset sizes:
         self.dataset_sizes = {split: len(dataloader.dataset) for split, dataloader in dataloaders.items()}
+
         # Following is needed for trapezoidal scheduler 
         self.num_batches_per_epoch = len(self.dataloaders['train'])
         self.total_batch_updates = self.num_batches_per_epoch * self.num_epoch
@@ -56,6 +62,20 @@ class trainer:
         # these are for trapezoidal scheduler
         if use_trap_scheduler:
             self._setup_trapezoid_scheduler()
+
+        # These are for the case when one wants to freeze the vit-backbone for a few epochs initially
+        self.want_backbone_frozen_intially = want_backbone_frozen_intially
+        if self.want_backbone_frozen_intially:
+            if freeze_epochs is None:
+                raise ValueError("freeze_epochs must be provided if want_backbone_frozen_intially=True")
+            self.freeze_epochs = freeze_epochs
+            # freeze backbone initially
+            self.freeze_backbone() #DEFINE THESE FUNCTIONS
+            self.reinit_optimizer(head_only=True)#DEFINE THESE FUNCTIONS
+        else:
+            self.freeze_epochs = None
+            # train all parameters from the start
+            self.reinit_optimizer(head_only=False)
 
         ## error terms:
         self.train_error_epoch_list = []
@@ -81,10 +101,38 @@ class trainer:
         else:
             return max(0.0, float(self.total_batch_updates - batch_step) / max(1, self.decay_steps))
 
+    ## --- NEXT 3 FUNCS are to help with initial freezing and later unfreezing of the vit backbone
+    def freeze_backbone(self):
+        for p in self.model.backbone_parameters:
+            p.requires_grad = False
+
+    def unfreeze_backbone(self):
+        for p in self.model.backbone_parameters:
+            p.requires_grad = True
+    
+    def reinit_optimizer(self, head_only=False):
+        OptimClass = type(self.optimizer)
+        if head_only:
+            params = self.model.head_parameters
+            lr = self.lr_groups[1]  # head LR
+            self.optimizer = OptimClass([{"params": params, "lr": lr}])
+        else:
+            params = [
+                {"params": self.model.backbone_parameters, "lr": self.lr_groups[0]},
+                {"params": self.model.head_parameters, "lr": self.lr_groups[1]}
+            ]
+            self.optimizer = OptimClass(params)
+
 
     ## these are the train, train_epoch and val_epoch functions we need:
     def train(self, k=1):
         for epoch in tqdm(range(self.num_epoch), desc="Epochs"):
+            ## unfreeze after freeze_epochs
+            if self.want_backbone_frozen_intially and epoch == self.freeze_epochs:
+                print(f" Unfreezing backbone at epoch {epoch+1}")
+                self.unfreeze_backbone()
+                self.reinit_optimizer(head_only=False)
+
             # train and validation step for i'th epoch
             avg_epoch_train_loss = self.train_epoch(epoch=epoch)
             avg_epoch_val_loss, avg_epoch_val_dice, avg_epoch_val_iou = self.val_epoch()# unpack all three values returned by val_epoch
