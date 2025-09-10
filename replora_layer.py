366bbeb4b832520786c33d6bf747eaa35282c2af
neelkanthrawat <neelkanthrawat3992@gmail.com>
2025-09-10 23:58:06 +0200

    correction in forward pass

--- replora_layer.py
+++ replora_layer.py
@@ -17,10 +17,10 @@ class RepLoraLayer(nn.Module):
         # build lora A, B layers
         self.A_gen = A_gen
         self.B_gen = B_gen
-        self.A, self.B = self._make_lora()
 
-    def _make_lora(self):
-        """Create LoRA A and B layers initialized from generators."""
+    def forward(self, x):
+        base_out = self.wt(x)
+
         A_q, A_v = self.A_gen()
         B_q, B_v = self.B_gen()
 
@@ -29,23 +29,5 @@ class RepLoraLayer(nn.Module):
         else:
             A_tensor, B_tensor = A_v, B_v
 
-        in_dim, r = A_tensor.shape   # (in_features, rank)
-        _, out_dim = B_tensor.shape  # (rank, output_feature)
-        assert out_dim == in_dim, "out_dim should be same as in_dim."
-        #print(f"shape of A_tensor is: {A_tensor.shape} and B_tensor is: {B_tensor.shape}")
-
-        A_layer = nn.Linear(in_dim, r, bias=True)
-        B_layer = nn.Linear(r, out_dim, bias=True)
-        #print(f" A_layer is: {A_layer} and B_layer is: {B_layer}")
-
-        with torch.no_grad():
-            A_layer.weight.copy_(A_tensor.T)  # nn.Linear expects (out_features, in_features)
-            B_layer.weight.copy_(B_tensor.T)
-
-        return A_layer, B_layer
-
-    def forward(self, x):
-        base_out = self.wt(x) # (batch_size, 197, 768)
-        #print(f"x shape is: {x.shape} and base_out's shape is: {base_out.shape}")
-        delta = self.B(self.A(x))
+        delta = (x @ A_tensor) @ B_tensor
         return base_out + (self.alpha / self.rank) * delta
