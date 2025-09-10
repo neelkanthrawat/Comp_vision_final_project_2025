5d04b62ae695ab8bfe3bfb4d5021f204b06bbec2
neelkanthrawat <neelkanthrawat3992@gmail.com>
2025-09-10 17:04:50 +0200

    fix typo while imploring serialloralayer

--- serial_lora_vit.py
+++ serial_lora_vit.py
@@ -3,7 +3,7 @@ import torch
 from torch import Tensor
 import math
 from safetensors.torch import save_file, load_file
-from serial_lora import SerialLoraLayer  # your separate Serial LoRA implementation
+from Serial_lora_layer import SerialLoraLayer  # your separate Serial LoRA implementation
 
 class SerialLoraVit(nn.Module):
     """ 
